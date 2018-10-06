dcade62af6c2f26d791dc5176c5b9958dbdd8e02
dwikivio <43898457+dwikivio@users.noreply.github.com>
2018-10-06 10:12:02 +0700

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -3,7 +3,7 @@ var app = {
     // Application Constructor
     initialize: function() {
     
-        var url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+        var url = "https://api.nasa.gov/planetary/apod?api_key=slBgrzLLXI4dkCueBB4sM3Jns9vtzzegXXNCad5S";
         
         $("#loadImage").click(function(){
             $.ajax({
